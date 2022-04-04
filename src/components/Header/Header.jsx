484800d7be16adb64f0bab19ea4975bf1cd7f49a
Cristopher Yang <dg.cris.yang@gmail.com>
2022-04-03 21:50:08 -0300

    fixed navbar: user and cart button missing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,13 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
 import SessionBuy from "./SessionBuy";
 import HomeLogo from './HomeLogo'
-import { padding } from '@mui/system';
 
 const pages = ['Home', 'Catalogue', 'Custom', 'Gallery', 'Contact'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-
 
 export default function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -95,52 +89,9 @@ export default function Header() {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px'}}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          <SessionBuy/>
         </Toolbar>
       </Container>
     </AppBar>
   );
 };
-
-
-/* export default function Header() {
-  return (
-    <>
-      <Box sx={{ bgcolor: "secondary.dark" }}>
-        <header className="header">
-          <HomeLogo />
-          <Nav />
-          <SessionBuy />
-        </header>
-      </Box>
-    </>
-  );
-} */
